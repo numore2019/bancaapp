c89d44475211c604f8d81eb2fa91a994ebfafaf1
numore2019 <rck_orellana7@hotmail.com>
2021-02-18 13:13:30 -0600

    nuevaact

--- src/main/java/com/bancoagricola/bancamovil/models/services/FacturaServiceImpl.java
+++ src/main/java/com/bancoagricola/bancamovil/models/services/FacturaServiceImpl.java
@@ -1,5 +1,41 @@
 package com.bancoagricola.bancamovil.models.services;
 
-public class FacturaServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bancoagricola.bancamovil.models.dao.IFacturaDao;
+import com.bancoagricola.bancamovil.models.entities.Factura;
+
+@Service
+public class FacturaServiceImpl implements IFacturaService{
+	
+	@Autowired
+	private IFacturaDao facturaDao;
+
+	@Override
+	public List<Factura> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Factura findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Factura save(Factura cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
